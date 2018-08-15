1c229908752178178baa16c5e1d72e78394da66f
PoundJira <59070026@it.kmitl.ac.th>
2018-08-15 12:03:07 +0700

    test Healthy 15/07/18

---
+++ app/src/main/java/com/example/lab203_19/healty/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lab203_19.healty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_login);
+    }
+}
